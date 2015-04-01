5203cc698c2726007ea1a919e5bff9e86ea192f6
chrislsavage <billing@hcproject.org>
2015-04-01 19:58:45 -0400

    complete

---
+++ First_Reverse.js
@@ -0,0 +1,10 @@
+function FirstReverse(str) { 
+
+
+  return str.split("").reverse().join(""); 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstReverse(readline());   
