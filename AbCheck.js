1913f27cde9cf9759ee30fc5ac8f888345a11ffe
chrislsavage <billing@hcproject.org>
2015-04-15 17:42:03 -0400

    Complete

---
+++ AbCheck.js
@@ -0,0 +1,13 @@
+function ABCheck(str) { 
+  for (var i = 0; i < str.length; i++) {
+    if (str.charAt(i) == 'a' && str.charAt(i+4) == 'b') return true;
+    if (str.charAt(i) == 'b' && str.charAt(i+4) == 'a') return true;
+  }
+  // code goes here  
+  return false; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+ABCheck(readline());   
