4f73a27a86f0c44af2ec8376b1689b34c18557d3
chrislsavage <billing@hcproject.org>
2015-04-17 12:21:21 -0400

    Complete

---
+++ VowelCount.js
@@ -0,0 +1,14 @@
+var vowels = "aeiou";
+var count = 0;
+function VowelCount(str) { 
+for (i=0; i<str.length; i++) {
+  if (vowels.indexOf(str[i]) != -1) {
+  count++;
+  }
+}
+  return count;
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+VowelCount(readline());                            
