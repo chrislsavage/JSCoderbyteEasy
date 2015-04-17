12ea4479b9770a5a0f8274731a7794fc222dc8c0
chrislsavage <billing@hcproject.org>
2015-04-17 12:15:05 -0400

    Complete

---
+++ AlphabetSoup.js
@@ -0,0 +1,9 @@
+function AlphabetSoup(str) { 
+    str = str.split("").sort().join("");
+    return str;
+}
+
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+AlphabetSoup(readline());           
