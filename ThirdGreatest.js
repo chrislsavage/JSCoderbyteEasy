ba2a6e131e5f290ac79a94574053606e480493c4
chrislsavage <billing@hcproject.org>
2015-04-20 16:53:14 -0400

    Complete

--- ThirdGreatest.js
+++ ThirdGreatest.js
@@ -1,11 +1,12 @@
 function ThirdGreatest(strArr) { 
 strArr.sort(function(a, b){
-  return a.length - b.length;
+  return b.length - a.length; // ASC -> a - b; DESC -> b - a
+});  
   console.log(strArr);
-  var length = strArr[strArr.length - 3];
+  var length = strArr[strArr.length - 1];
   return length;
 }
    
 // keep this function call here 
 // to see how to enter arguments in JavaScript scroll down
-ThirdGreatest(readline());           
+ThirdGreatest(readline());  
