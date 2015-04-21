ce6f3df3a57b4a044247923a41c6ce7efe6b04ca
chrislsavage <billing@hcproject.org>
2015-04-21 09:46:10 -0400

    Complete

---
+++ WordCount.js
@@ -0,0 +1,10 @@
+function WordCount(str) { 
+
+str = str.split(" ");
+  return str.length; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+WordCount(readline());  
